c145ac38142f0c11a854653894c0bdaafac98e2f
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-02-19 23:33:48 +0200

    Add static categories - brands and bring products from API and Showing it

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ShopRoutingModule
   ],
   exports: [
     ShopComponent
